eb6407e6e1fca2db0797f97dbd300bab7fb2578d
Felhasznalo <mihokadam24@gmail.com>
2025-01-29 22:14:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // interaktív négyzet és félpiramis rajzoló
+        // kérjük be az adatokat a felhasználótól (négyzet/piramis mérete és a megjelenítendő karakter
+        
+    }
+}
